9ea1114f16cbd158486274c1a6abbcd6545690a2
Vikram <alexgiantseed@gmail.com>
2018-11-01 06:42:18 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,9 @@
 Rails.application.routes.draw do
-  get '/tutee_signup', to: 'tutees#new'
-
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-  
-  get 'static_pages/about'
-  
   root 'static_pages#home'
-
-  post '/signup',  to: 'tutees#create'
-
-  resources :tutees
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup', to: 'tutees#signup'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
